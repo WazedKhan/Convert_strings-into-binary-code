1350b7a5f7cfaa672e870ab3782bbacdd62eac81
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2020-10-13 22:20:52 +0600

    first commit

---
+++ stringTOBinary.py
@@ -0,0 +1,21 @@
+value = 'My name A'
+string = []
+for i in value.split(' '):
+    string.append(i)
+
+asci = []
+for i in string:
+    asci.extend(ord(j) for j in i)
+print(asci)
+
+def asciTobin(numbers):
+    value = []
+    for n in numbers:
+        value.append(bin(n).replace('0b',''))
+    return value
+
+f_value = asciTobin(asci)
+print('Binary value of string:')
+for val in f_value:
+    print(str(val)+' ', end='')
+
